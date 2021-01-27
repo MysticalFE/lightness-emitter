8aa8326740153ba8d45d1fbc9d9b4eb46965a405
贾祥凯 <xiangkai.jia@eoitek.com>
2021-01-27 13:33:06 +0800

    完善lightEmitter类方法

--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
 export type EventType = string;
 export type Callback<T = any> = (event: T) => void;
-
-
+export type EventTypeMap = Map<EventType, Array<Callback>>;
 export interface Emitter {
+  all: EventTypeMap;
   on<T = any>(type: EventType, callback: Callback<T>): void;
   once<T = any>(type: EventType, callback: Callback<T>): void;
-  emit<T = any>(type: EventType, ctx: T): void;
+  emit<T = any>(type: EventType, ctx?: T): void;
   off<T = any>(type: EventType, callback: Callback<T>): void;
 }
-
-class lightEmitter implements Emitter {
+export default class lightEmitter implements Emitter {
+  public all: EventTypeMap = new Map()
   public on<T = any>(type: EventType, callback: Callback<T>) {
-  
+    let cbs = this.all.get(type);
+    const isCallbackExist = cbs && cbs.push(callback);
+    if(!isCallbackExist) this.all.set(type, [callback]);
+    return this;
   }
   public once<T = any>(type: EventType, callback: Callback<T>) {
-  
+    this.off(type, callback);
+    this.on(type, callback);
   }
-  public emit<T = any>(type: EventType, ctx: T) {
-
+  public emit<T = any>(type: EventType, ctx?: T) {
+    const cbs = this.all.get(type) || [];
+    [...cbs].map(cb => { cb(ctx) });
+    return this;
   }
   public off<T = any>(type: EventType, callback: Callback<T>) {
-  
+    const cbs = this.all.get(type);
+    if(cbs) cbs.splice(cbs.indexOf(callback), 1);
+    return this;
   }
 }
-export default new lightEmitter();
